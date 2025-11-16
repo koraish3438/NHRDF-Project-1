0ec8528f9f37786a36d22c226951c49ea8d0499d
koraish3438 <koraishmdkajamall38@gmail.com>
2025-11-16 13:38:00 +0600

    Ready project 2nd time

--- app/src/main/java/com/example/nhrdfproject1/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/nhrdfproject1/ui/main/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.nhrdfproject1.R
 import com.example.nhrdfproject1.databinding.ActivityMainBinding
 import com.example.nhrdfproject1.ui.adapter.ProductAdapter
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var productAdapter: ProductAdapter
     private lateinit var productViewModel: ProductViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,12 +24,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         productViewModel = ViewModelProvider(this)[ProductViewModel::class.java]
 
         productAdapter = ProductAdapter(emptyList())
         binding.recyclerView.apply {
             adapter = productAdapter
-            layoutManager = LinearLayoutManager(this@MainActivity)
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
             setHasFixedSize(true)
         }
 
